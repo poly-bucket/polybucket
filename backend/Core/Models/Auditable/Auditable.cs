676adfcfdea244cd3af2f187875e2bec4f7bf074
CodyFraker <codyfraker95@gmail.com>
2024-07-29 17:05:21 -0400

    Setup backend skeleton

---
+++ backend/Core/Models/Auditable/Auditable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.Auditable
+{
+    public abstract class Auditable
+    {
+        public long CreatedById { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public long UpdatedById { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public long DeletedById { get; set; }
+        public DateTime DeletedAt { get; set; }
+    }
+}
---
+++ backend/Core/Models/Users/UserLogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.Users
+{
+    public class UserLogin
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
